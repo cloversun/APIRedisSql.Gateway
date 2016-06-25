70ed2d61e0a085492899e6cab07359bf536b598c
hsun <cloversun@163.com>
2016-06-25 16:30:52 +0800

    Add redis test

---
+++ APIRedisSql.Gateway.Test/SqlDalTest.cs
@@ -0,0 +1,34 @@
+﻿
+using NUnit.Framework;
+using APIRedisSql.Gateway.Model;
+
+namespace APIRedisSql.Gateway.Test
+{
+    [TestFixture]
+    public class SqlDalTest
+    {
+
+        [Test]
+        public void GetPeriodSortingInfoSqlToRedisTest()
+        {
+            var peroidLists = SqlDal.GetPeriodAAModelList();
+            if (peroidLists.Count > 0)
+            {
+                Redishelper.PushList<AAGetModel>("SortingList", peroidLists);
+            }
+
+        }
+        [Test]
+        public void InsertSuccessedSortingInfoTest()
+        {
+            var BBModelList = TestHelper.CreateBBModelList();
+
+            var preCount = SqlDal.GetRowNumberForTable("Logistics.JinFengSuccessedSorting");
+            SqlDal.InsertPostInfo(BBModelList);
+            var afterCount = SqlDal.GetRowNumberForTable("Logistics.JinFengSuccessedSorting");
+
+            Assert.AreEqual(preCount + 2, afterCount);
+        }
+
+    }
+}
